2a36bee8234b163bf2f8e753fa7d402cacfa160a
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2018-07-24 17:17:55 -0500

    provide dummy extra run opts, in case none are provided on the cli

--- run.sh
+++ run.sh
@@ -62,10 +62,11 @@ run_embark_docker () {
         fi
     fi
 
+    local dummy="-e __embark_docker_runsh"
     local EMBARK_DOCKER_MOUNT_SOURCE=${EMBARK_DOCKER_MOUNT_DIR:-$PWD}
     local EMBARK_DOCKER_MOUNT_TARGET=${EMBARK_DOCKER_MOUNT_DIR:-/dapp}
+    local EMBARK_DOCKER_EXTRA_RUN_OPTS=${EMBARK_DOCKER_EXTRA_RUN_OPTS:-$dummy}
     local EMBARK_DOCKER_IMAGE=${EMBARK_DOCKER_IMAGE:-statusim/embark}
-    local EMBARK_DOCKER_EXTRA_RUN_OPTS=${EMBARK_DOCKER_EXTRA_RUN_OPTS:-""}
     local EMBARK_DOCKER_TAG=${EMBARK_DOCKER_TAG:-latest}
 
     docker run \
